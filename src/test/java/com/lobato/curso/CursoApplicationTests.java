87c387e1486a1c27d9c78dda2482a5eed158af76
Guilherme Lobato <guilherme.lobato09@gmail.com>
2023-02-04 11:19:12 -0300

    Project created

---
+++ src/test/java/com/lobato/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobato.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
