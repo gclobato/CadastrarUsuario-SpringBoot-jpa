1fccd4891d8cb4d63806030716070ae9caa09609
Guilherme Lobato <guilherme.lobato09@gmail.com>
2023-02-25 10:27:32 -0300

    Service layer, component registration

--- src/main/java/com/lobato/curso/resources/UserResource.java
+++ src/main/java/com/lobato/curso/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.lobato.curso.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lobato.curso.entities.User;
+import com.lobato.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findALL(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "(61)9.9999-9999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findALL(){
+		List<User> list = service.findAll(); 
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
