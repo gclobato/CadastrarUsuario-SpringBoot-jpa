b7df98c50b634984cd04cf0ee549436672c823bd
Guilherme Lobato <guilherme.lobato09@gmail.com>
2023-05-08 21:18:23 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/lobato/curso/config/TestConfig.java
+++ src/main/java/com/lobato/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.lobato.curso.entities.Category;
 import com.lobato.curso.entities.Order;
+import com.lobato.curso.entities.OrderItem;
 import com.lobato.curso.entities.Product;
 import com.lobato.curso.entities.User;
 import com.lobato.curso.entities.enums.OrderStatus;
@@ -17,6 +18,7 @@ import com.lobato.curso.repositories.CategoryRepository;
 import com.lobato.curso.repositories.OrderRepository;
 import com.lobato.curso.repositories.ProductRepository;
 import com.lobato.curso.repositories.UserRepository;
+import com.lobato.curso.repositories.OrderItemRepository;
 
 @Configuration
 @Profile("test")
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +73,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 }
--- src/main/java/com/lobato/curso/entities/Order.java
+++ src/main/java/com/lobato/curso/entities/Order.java
@@ -2,7 +2,9 @@ package com.lobato.curso.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.lobato.curso.entities.enums.OrderStatus;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_order")
@@ -33,6 +36,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>(); 
 	
 	public Order() {
 	}
