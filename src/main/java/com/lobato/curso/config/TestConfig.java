c8180f96851208fd50337b2a3f0d50aa3827244d
Guilherme Lobato <guilherme.lobato09@gmail.com>
2023-05-12 21:44:06 -0300

    Payment, one-to-one association

--- src/main/java/com/lobato/curso/config/TestConfig.java
+++ src/main/java/com/lobato/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lobato.curso.entities.Category;
 import com.lobato.curso.entities.Order;
 import com.lobato.curso.entities.OrderItem;
+import com.lobato.curso.entities.Payment;
 import com.lobato.curso.entities.Product;
 import com.lobato.curso.entities.User;
 import com.lobato.curso.entities.enums.OrderStatus;
@@ -81,7 +82,8 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
-		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1); 
 		
 	}
 }
